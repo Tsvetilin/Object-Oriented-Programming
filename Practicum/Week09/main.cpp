437753fc95d405c4ccb42f1bbcbd295d134156d8
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2025-04-29 10:27:42 +0300

    Week 9 - Exercise 2

--- Practicum/Week09/main.cpp
+++ Practicum/Week09/main.cpp
@@ -1,119 +1,86 @@
-#include<iostream>
-#include<stdexcept>
-#include<cstring>
+#include <iostream>
+#include "Picture.h"
+#include "Pixel.h"
+#include "Gallery.h"
 
-#include "String.h"
-#include "Order.h"
-#include "Restaurant.h"
-#include "Foodpanda.h"
+int main() {
+    try {
+        Pixel** img = new Pixel * [2];
+        for (int i = 0; i < 2; ++i) {
+            img[i] = new Pixel[2];
+        }
 
-int main(){
-    //Check after writing String.h
-	/*String str1("fish", 5);
-	str1.print();
+        img[0][0] = Pixel(100, 150, 200);
+        img[0][1] = Pixel(50, 80, 90);
+        img[1][0] = Pixel(200, 20, 50);
+        img[1][1] = Pixel(90, 170, 40);
 
-	String str2("kiwi");
-	str2.print();
+        Picture picture((const Pixel**)img, 2, 2);
 
-	bool t = str1 == str2;
-	std::cout << t << std::endl;*/
+        Gallery gallery;
+        gallery.addNewPicture(picture, false);
 
-    //check after writing class Order
-    /*try {
-        String product1("Pizza");
-        String product2("Burger");
-        String product3("Pasta");
+        std::cout << "Gallery information:" << std::endl;
+        gallery.printGalleryInfo();
 
-        String products[] = { product1, product2 };
+        std::cout << "Original picture:" << std::endl;
+        picture.printPicture();
 
-        Order order("Italiano Restaurant", 2, products, 2);
+        if (picture.isColourful()) {
+            std::cout << "The picture is colorful!" << std::endl;
+        }
 
-        order.printOrder();
+        Pixel** grayscaleImg = picture.grayscale();
+        std::cout << "Grayscale picture:" << std::endl;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                std::cout << "(" << (int)grayscaleImg[i][j].getRedColourValue() << ", " << (int)grayscaleImg[i][j].getGreenColourValue() << ", " << (int)grayscaleImg[i][j].getBlueColourValue() << ") ";
+            }
+            std::cout << std::endl;
+        }
 
-        order.addNewProduct(product3);
+        std::cout << std::endl;
 
-        std::cout << "After adding a new product:\n";
-        order.printOrder();
+        Pixel** monochromeImg = picture.monochrome();
+        std::cout << "Monochrome picture:" << std::endl;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                std::cout << "(" << (int)monochromeImg[i][j].getRedColourValue() << ", " << (int)monochromeImg[i][j].getGreenColourValue() << ", " << (int)monochromeImg[i][j].getBlueColourValue() << ") ";
+            }
+            std::cout << std::endl;
+        }
 
-        size_t deliveryTime = order.calcDeliveryTime();
-        std::cout << "Calculated delivery time: " << deliveryTime << " minutes" << std::endl;
+        std::cout << std::endl;
+
+        Pixel** negativeImg = picture.negative();
+        std::cout << "Negative picture:" << std::endl;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                std::cout << "(" << (int)negativeImg[i][j].getRedColourValue() << ", " << (int)negativeImg[i][j].getGreenColourValue() << ", " << (int)negativeImg[i][j].getBlueColourValue() << ") ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        for (int i = 0; i < 2; ++i) {
+            delete[] img[i];
+            delete[] grayscaleImg[i];
+            delete[] monochromeImg[i];
+            delete[] negativeImg[i];
+        }
+        delete[] img;
+        delete[] grayscaleImg;
+        delete[] monochromeImg;
+        delete[] negativeImg;
     }
     catch (const std::exception& e) {
-        std::cerr << "An exception occurred: " << e.what() << std::endl;
-    }*/
-
-    //check after Restaurant class
-    /*String product1("Pizza", 5);
-    String product2("Burger", 6);
-    String product3("Pasta", 5);
-
-    String products[] = { product1, product2, product3 };
-
-    Restaurant restaurant("FoodPlace", products, 3, 5);
-
-    std::cout << "Restaurant: " << restaurant.getName() << std::endl;
-    std::cout << "Products offered by the restaurant:" << std::endl;
-    for (size_t i = 0; i < restaurant.getProductListSize(); ++i) {
-        std::cout << "  - " << restaurant.getProductList()[i].getString() << std::endl;
+        std::cerr << "An error occurred: " << e.what() << std::endl;
     }
-
-    String orderProduct1("Pizza", 5);
-    String orderProduct2("Pasta", 5);
-    String orderProducts[] = { orderProduct1, orderProduct2 };
-
-    Order order("FoodPlace", 2, orderProducts, 5);
-
-    std::cout << "Order details:" << std::endl;
-    order.printOrder();
-
-    try {
-        restaurant.receiveOrder(order);
-    }
-    catch (const std::invalid_argument& e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
-
-    String newProduct("Salad", 5);
-    restaurant.addNewProductToList(newProduct);
-
-    std::cout << std::endl;
-
-    std::cout << "Updated product list after adding a new product:" << std::endl;
-    for (size_t i = 0; i < restaurant.getProductListSize(); ++i) {
-        std::cout << "  - " << restaurant.getProductList()[i].getString() << std::endl;
-    }
-
-    String invalidOrderProduct("Sushi", 5);
-    String invalidOrderProducts[] = { invalidOrderProduct };
-
-    Order invalidOrder("FoodPlace", 1, invalidOrderProducts, 5);
-
-    try {
-        restaurant.receiveOrder(invalidOrder);
-    }
-    catch (const std::invalid_argument& e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }*/
-
-    String products[] = { String("Burger"), String("Fries"), String("Cola") };
-
-    Restaurant myRestaurant("BestBurgers", products, 3, 5);
-
-    Foodpanda app;
-    Restaurant restaurantList[] = { myRestaurant };
-    app.setRestaurants(restaurantList, 1, 3);
-
-    String orderProducts[] = { String("Burger"), String("Cola") };
-
-    Order myOrder("BestBurgers", 2, orderProducts, 4);
-
-    try {
-        app.addNewOrder(myOrder);
-        std::cout << "Order added successfully!" << std::endl;
-    }
-    catch (const std::exception& e) {
-        std::cout << "Failed to add order: " << e.what() << std::endl;
+    catch (...) {
+        std::cerr << "An unknown error occurred!" << std::endl;
     }
 
     return 0;
 }
+
