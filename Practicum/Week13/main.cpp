7f79695cb9bccbda92ff7ba1bbd064b0560146e6
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2025-05-19 21:39:21 +0300

    Exercise 1

--- Practicum/Week13/main.cpp
+++ Practicum/Week13/main.cpp
@@ -1,31 +1,15 @@
-#include <iostream>
-#include "Company.h"
-#include "Worker.h"
-#include "Trainee.h"
-#include "PaidTrainee.h"
+﻿#include "SharedPtr.h"
 
 int main() {
-    Company c;
+	try {
+		SharedPtr<int, 2> p1(new int(42));
+		SharedPtr<int, 2> p2 = p1;
+		SharedPtr<int, 2> p3 = p1;
 
-    Worker w1("Alice", 28, 2000, 10);
-    Worker w2("Bob", 40, 2500, 24);
+		std::cout << *p1 << std::endl;
 
-    Trainee t1("Charlie", 23, 1000, 2);
-    Trainee t2("Dana", 23, 1100, 2);
-
-    PaidTrainee pt1("Eve", 24,1800, 5, 31, 70.05);
-
-    c.addWorker(w1);
-    c.addWorker(w2);
-    c.addTrainee(t1);
-    c.addTrainee(t2);
-    c.addPaidTrainee(pt1);
-
-    std::cout << "Average salary: " << c.getAverageSalary() << std::endl;
-
-    double target = 3100;
-    std::cout << "Two-sum for " << target << ": "
-        << (c.twoSum(target) ? "Exists" : "Does NOT exist") << std::endl;
-
-    return 0;
+	}
+	catch (const std::exception& ex) {
+		std::cout << "Exception: " << ex.what() << std::endl;
+	}
 }
